80061fc0c7f298ddd7b23ad57c3507073e8eeeb6
Dhruv <63149531+dhr7v@users.noreply.github.com>
2026-07-11 17:43:30 +0200

    Update sleep-control to version 1.0.1

--- Casks/sleep-control.rb
+++ Casks/sleep-control.rb
@@ -1,8 +1,8 @@
 cask "sleep-control" do
-  version "1.0.0"
-  sha256 "09268f73495242d39697cb753437102aa45b55ca3b8f54a9e50005496e095322"
+  version "1.0.1"
+  sha256 "4d89baec72caebcd21c06303e5af03625790a9493ad338af3510f38538f5a46b"
 
-  url "https://github.com/dhr7v/sleep-control/releases/download/1.0.0/SleepControl-1.0.0.zip",
+  url "https://github.com/dhr7v/sleep-control/releases/download/1.0.0/SleepControl-1.0.1.zip",
       verified: "github.com/dhr7v/sleep-control/"
   name "Sleep Control"
   desc "GUI tool to enable/disable lid-close sleep via pmset"
